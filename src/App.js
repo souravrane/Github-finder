695f5d5a62c4661b537a570933919a085f990163
souravrane <sourav1121997@gmail.com>
2022-04-02 15:18:24 +0530

    setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
-import logo from "./logo.svg";
-import "./App.css";
-
 function App() {
     return (
-        <div className="App">
+        <div className="bg-purple-500">
             <h1 className="text-3xl font-bold underline">Hello world!</h1>
+            <button className="btn">click</button>
         </div>
     );
 }
