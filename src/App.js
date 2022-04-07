8abbfc05e9f80b6e34279ad5177184b8c63a5fa7
souravrane <sourav1121997@gmail.com>
2022-04-07 08:29:56 +0530

    Alert component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Footer from "./components/layout/Footer";
 import NavBar from "./components/layout/NavBar";
+import Alert from "./components/layout/Alert";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import NotFound from "./pages/NotFound";
@@ -16,6 +17,7 @@ function App() {
                         <NavBar />
 
                         <main className="container mx-auto px-3 pb-12">
+                            <Alert />
                             <Routes>
                                 <Route path="/" element={<Home />} />
                                 <Route path="/about" element={<About />} />
