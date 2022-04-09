bd2822b641f52171869ca5c73331486009959d4d
souravrane <sourav1121997@gmail.com>
2022-04-09 22:09:28 +0530

    Basic repo list

--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -3,13 +3,16 @@ import { FaCodepen, FaStore, FaUserFriends, FaUsers } from "react-icons/fa";
 import { useParams, Link } from "react-router-dom";
 import Spinner from "../components/layout/Spinner";
 import GithubContext from "../context/github/GithubContext";
+import RepoList from "../components/repos/RepoList";
 
 function User() {
-    const { getUser, user, loading } = useContext(GithubContext);
+    const { getUser, user, loading, getUserRepos, repos } =
+        useContext(GithubContext);
     const params = useParams();
 
     useEffect(() => {
         getUser(params.login);
+        getUserRepos(params.login);
     }, []);
 
     const {
@@ -169,6 +172,8 @@ function User() {
                         </div>
                     </div>
                 </div>
+
+                <RepoList repos={repos} />
             </div>
         </>
     );
